40dcb381e81994e07efa78d6008e35428e7d76a7
rnazmutdinov <rnazmutdinov@vivid.money>
2022-03-20 14:27:25 +0300

    add new fields

--- src/main/kotlin/com/meetme/meeting/Meeting.kt
+++ src/main/kotlin/com/meetme/meeting/Meeting.kt
@@ -3,6 +3,8 @@ package com.meetme.meeting
 import com.meetme.iterest.Interest
 import com.meetme.medialink.MediaLink
 import com.meetme.auth.User
+import java.time.Instant
+import java.util.Date
 import javax.persistence.*
 
 @Entity(name = "Meeting")
@@ -19,7 +21,22 @@ data class Meeting(
     var description: String? = null,
 
     @Column(name = "photo_url")
-    var photoUrl: String? = null,
+    var imageUrl: String? = null,
+
+    @Column(name = "is_online")
+    var isOnline: Boolean = false,
+
+    @Column(name = "start_date")
+    var startDate: Date = Date.from(Instant.now()),
+
+    @Column(name = "end_date")
+    var endDate: Date? = null,
+
+    @Column(name = "is_private")
+    var isPrivate: Boolean = false,
+
+    @Column(name = "location")
+    var location: String? = null,
 
     @ManyToOne(targetEntity = User::class)
     @JoinColumn(name = "user_id")
