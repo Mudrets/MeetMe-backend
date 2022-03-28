0a4d93bf5b588be48c0c2bf174dc3efe22c0bd64
rnazmutdinov <rnazmutdinov@vivid.money>
2022-03-28 17:42:06 +0300

    make endDate nullable

--- src/main/kotlin/com/meetme/dto/meeting/EditMeetingDto.kt
+++ src/main/kotlin/com/meetme/dto/meeting/EditMeetingDto.kt
@@ -4,7 +4,7 @@ data class EditMeetingDto(
     val name: String,
     val description: String,
     val startDate: String,
-    val endDate: String,
+    val endDate: String? = null,
     val isOnline: Boolean,
     val locate: String,
     val maxNumberOfParticipants: Int,
