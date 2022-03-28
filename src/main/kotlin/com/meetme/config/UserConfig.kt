474b90ccf9b11f351a2ae5b56bfc705dfafcd4c7
rnazmutdinov <rnazmutdinov@vivid.money>
2022-03-28 16:27:33 +0300

    add meetings in config

--- src/main/kotlin/com/meetme/config/UserConfig.kt
+++ src/main/kotlin/com/meetme/config/UserConfig.kt
@@ -2,6 +2,8 @@ package com.meetme.config
 
 import com.meetme.auth.User
 import com.meetme.auth.UserDao
+import com.meetme.meeting.Meeting
+import com.meetme.meeting.MeetingDao
 import org.springframework.boot.CommandLineRunner
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -10,12 +12,34 @@ import org.springframework.context.annotation.Configuration
 class UserConfig {
 
     @Bean
-    fun commandLineRunner(userDao: UserDao) = CommandLineRunner {
-            val alex = User(email = "alex", password = "123456")
-            val mariam = User(email = "mariam", password = "123456")
+    fun userCommandLineRunner(userDao: UserDao, meetingDao: MeetingDao) = CommandLineRunner {
+        val alex = User(email = "alex", password = "123456")
+        val mariam = User(email = "mariam", password = "123456")
 
-            userDao.saveAll(
-                    listOf(alex, mariam)
-            )
-        }
+        val meeting1 = Meeting(
+            name = "My birthday",
+            description = "lol kek cheburek",
+            endDate = "03-22-2022 23:59",
+            startDate = "03-22-2022 00:00",
+            admin = mariam,
+        )
+        mariam.meetings.add(meeting1)
+
+        val meeting2 = Meeting(
+            name = "Курсач",
+            description = "очень очень сложна",
+            endDate = "03-26-2022 23:59",
+            startDate = "03-26-2022 00:00",
+            admin = alex,
+        )
+        alex.meetings.add(meeting2)
+
+        userDao.saveAll(
+            listOf(alex, mariam)
+        )
+
+        meetingDao.saveAll(
+            listOf(meeting1, meeting2)
+        )
+    }
 }
