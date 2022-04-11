41addfd02a03dc350e6150d75cf99b6efa38aa02
rnazmutdinov <rnazmutdinov@vivid.money>
2022-04-12 00:38:50 +0300

    replace image url to dto in FileController

--- src/main/kotlin/com/meetme/service/file/FileController.kt
+++ src/main/kotlin/com/meetme/service/file/FileController.kt
@@ -1,6 +1,16 @@
 package com.meetme.service.file
 
 import com.meetme.domain.dto.DataResponse
+import com.meetme.domain.dto.auth.UserDto
+import com.meetme.domain.dto.goup.GroupDto
+import com.meetme.domain.dto.meeting.MeetingDto
+import com.meetme.service.group.GroupService
+import com.meetme.service.group.mapper.GroupToGroupDto
+import com.meetme.service.meeting.MeetingService
+import com.meetme.service.meeting.mapper.MeetingToMeetingDto
+import com.meetme.service.user.UserService
+import com.meetme.service.user.mapper.UserToUserDto
+import com.meetme.util.Constants
 import com.meetme.util.tryExecute
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
@@ -8,7 +18,6 @@ import org.springframework.core.io.Resource
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
-import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
 
@@ -27,6 +36,24 @@ class FileController {
     @Qualifier("userFileStoreService")
     @Autowired
     private lateinit var userFileStoreService: FileStoreService<Long>
+
+    @Autowired
+    private lateinit var meetingService: MeetingService
+
+    @Autowired
+    private lateinit var groupService: GroupService
+
+    @Autowired
+    private lateinit var userService: UserService
+
+    @Autowired
+    private lateinit var meetingToMeetingDto: MeetingToMeetingDto
+
+    @Autowired
+    private lateinit var userToUserDto: UserToUserDto
+
+    @Autowired
+    private lateinit var groupToGroupDto: GroupToGroupDto
 
     private fun FileStoreService<*>.returnImage(fileName: String): ResponseEntity<Resource> {
         val inputStream = getImage(fileName)
@@ -55,26 +82,29 @@ class FileController {
     fun uploadMeetingImage(
         @RequestParam("image") image: MultipartFile,
         @PathVariable("meeting_id") meetingId: Long,
-    ): DataResponse<String> =
+    ): DataResponse<MeetingDto> =
         tryExecute {
             meetingFileStoreService.store(image, meetingId)
+            meetingToMeetingDto(meetingService.get(meetingId), Constants.NON_EXISTENT_USER_ID)
         }
 
     @PostMapping("/api/v1/user/{user_id}/image")
     fun uploadUserImage(
         @RequestParam("image") image: MultipartFile,
         @PathVariable("user_id") userId: Long,
-    ): DataResponse<String> =
+    ): DataResponse<UserDto> =
         tryExecute {
             userFileStoreService.store(image, userId)
+            userToUserDto(userService.get(userId))
         }
 
     @PostMapping("/api/v1/groups/{group_id}/image")
     fun uploadGroupImage(
         @RequestParam("image") image: MultipartFile,
         @PathVariable("group_id") groupId: Long,
-    ): DataResponse<String> =
+    ): DataResponse<GroupDto> =
         tryExecute {
             groupFileStoreService.store(image, groupId)
+            groupToGroupDto(groupService.get(groupId))
         }
 }
